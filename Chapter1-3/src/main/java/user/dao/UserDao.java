9e7827590b4b52ef908aebefe22767111104efce
hanna <gkssk4163@hanmail.net>
2021-11-02 16:19:39 +0900

    Chapter1-3 생성

---
+++ Chapter1-3/src/main/java/user/dao/UserDao.java
@@ -0,0 +1,47 @@
+package user.dao;
+
+import user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Connection c = getConnection();
+
+		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+
+		ps.executeUpdate();
+
+		ps.close();
+		c.close();
+	}
+
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Connection c = getConnection();
+
+		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+		ps.setString(1, id);
+
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+
+		rs.close();
+		ps.close();
+		c.close();
+
+		return user;
+	}
+
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		return DriverManager.getConnection(
+				"jdbc:mysql://localhost/springbook?useUnicode=true&characterEncoding=UTF-8", "spring", "book");
+	}
+}
